896d217af4d58ca395d715b15cc53e4f7396570c
Ricky <nguyenkhanglhp1605@gmail.com>
2019-10-09 13:52:51 -0400

    Unity changes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    // FixedUpdate is used when manipulating physics
+    void FixedUpdate ()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        //Debug.Log("Horizontal:" + horiz + "Vertical" + vert);
+
+        // Modify players velocity through the rigidbody2D
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = new Vector2(horiz*speed, vert*speed);
+	}
+}
